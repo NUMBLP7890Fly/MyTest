6f25ccff445b841c470ff0177775896712479141
zhangbo <zhangbo@qiniu.com>
2018-06-26 10:30:31 +0800

    add file

---
+++ git/test.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a bool = true
+	b := false
+	fmt.Println("a:", a, "b:", b)
+	c := a && b
+	fmt.Println("c:", c)
+	d := a || b
+	fmt.Println("d:", d)
+}
